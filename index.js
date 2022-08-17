a4be736df2034c25af435fe8b2fcce25912a521c
Artur Todeschini <artodeschini@gmail.com>
2022-08-17 17:52:21 -0300

    base de socket.io

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ let app = express();
 
 // devido ao socket.io utiliza o servidor nativo do node
 let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
-let socket = require('socket.io')(http);
+io.on('connection', (socket) => {
+    //console.log(socket);
+    //console.log(socket.id);
+    socket.on('boasvindas', (data) => {
+        console.log('executando boas vindas');
+        console.log(data);
+    });
+
+    socket.on('palavra', (data) => {
+        console.log('executando palavra');
+        console.log(data);
+
+        // respondendo do servidor para o cliente
+        socket.emit('resultado', data + ' resposta enviada em ' + new Date());
+    });
+});
 
 app.set('view engine', 'ejs');
 
